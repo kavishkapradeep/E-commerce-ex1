33231bfb77079298227f9587e70ec643c4abb569
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-07-10 13:49:29 +0530

    product display

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/Frontend_Assets/star_icon.png'
 import star_dull_icon from '../Assets/Frontend_Assets/star_dull_icon.png'
+import { useContext } from 'react'
+import { ShopContext } from '../../Context/ShopContext'
 
 const ProductDisplay = (props) => {
     const {product} =props;
+    const {addToCart} =useContext(ShopContext);
   return (
     <div className='productdisplay'>
         <div className="productdisplay-left">
@@ -48,7 +51,7 @@ const ProductDisplay = (props) => {
                     </div>
                 </div>
 
-                <button>ADD TO CART</button>
+                <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
             <p className="productdisplay-right-category">
                 <span>Category :</span> women t-shirt,croptop
             </p>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,15 +1,37 @@
 import React   from 'react'
 import all_product from '../Components/Assets/Frontend_Assets/all_product'
 import { createContext } from 'react'
+import { useState } from 'react'
 
 export const ShopContext =createContext(null)
 
-const ShopContextProvider = (props) =>{
-    const  contextValue ={all_product};
-
-    const getDefaultCart =()=>{
-        
+const getDefaultCart =()=>{
+    let cart ={};
+    for (let index = 0; index < all_product.length+1; index++) {
+        cart[index]=0;            
     }
+    return cart;
+}
+
+const ShopContextProvider = (props) =>{
+    const [cartItems,setCartItems]= useState(getDefaultCart());
+    
+    const addToCart =(itemId)=>{
+        setCartItems((prev)=>({
+            ...prev,[itemId]:prev[itemId]+1
+            
+        }));
+        console.log(cartItems);
+    }
+    
+    const removefromCart =(itemId)=>{
+        setCartItems((prev)=>({
+            ...prev,[itemId]:prev[itemId]-1
+        }))
+    }
+    
+    const  contextValue ={all_product,cartItems,addToCart,removefromCart};
+
 
     return (
         <ShopContext.Provider value={contextValue}>
