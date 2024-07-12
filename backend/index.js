ea346ba0a1af61c311c5726e104f8841c9d82af6
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-07-13 02:59:30 +0530

    post method done

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,8 @@ const jwt=require("jsonwebtoken");
 const multer =require("multer");
 const path =require("path");
 const cors =require("cors");
-const { error } = require("console");
+const { error, log } = require("console");
+const { type } = require("os");
 
 app.use(express.json());
 app.use(cors());
@@ -49,5 +50,59 @@ app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         sucess:1,
         image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+//Schema for Creating Products
+
+const Product =mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+    ,available:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async (req,res)=>{
+    const  product =new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("saved");
+    res.json({
+        sucess:true,
+        name:req.body.name,
     })
 })
