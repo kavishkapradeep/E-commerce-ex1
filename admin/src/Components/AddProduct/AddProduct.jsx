bbc999cbf2886b83cbf79116f175303ff7d07a9b
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-07-14 02:03:50 +0530

    data added server done

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -43,6 +43,16 @@ const AddProduct = () => {
         {
             product.image = responseData.image_url;
             console.log(product)
+            await fetch ('http://localhost:4000/addproduct',{
+                method:'POST',
+                headers:{
+                    Accept:'application/json'
+                    ,'Content-Type':'application/json'
+                },
+                body:JSON.stringify(product),
+            }).then((resp)=>resp.json()).then((data)=>{
+                data.sucess?alert("Product Added"):alert("Failed")
+            })
         }
        
    }
